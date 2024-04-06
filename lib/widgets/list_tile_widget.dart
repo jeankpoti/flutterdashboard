c0cd681394eda77c29e8e8ebe34bbbedb5495321
jeankpoti <kpotistar@gmail.com>
2024-04-05 21:24:30 -0500

    Add animation to session by country card on page load

--- lib/widgets/list_tile_widget.dart
+++ lib/widgets/list_tile_widget.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../constants/app_colors.dart';
 import '../constants/app_text_styles.dart';
 
-class ListileWidget extends StatelessWidget {
+class ListileWidget extends StatefulWidget {
   final String imageUrl;
   final String countryName;
   final String sessionValue;
@@ -21,23 +20,56 @@ class ListileWidget extends StatelessWidget {
   });
 
   @override
+  State<ListileWidget> createState() => _ListileWidgetState();
+}
+
+class _ListileWidgetState extends State<ListileWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0, end: widget.jaugeValue).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var w;
     return ListTile(
       leading: CircleAvatar(
-        backgroundImage: AssetImage(imageUrl),
+        backgroundImage: AssetImage(widget.imageUrl),
       ),
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            countryName,
+            widget.countryName,
             style: AppTextStyles.body,
           ),
           Row(
             children: [
               Text(
-                sessionValue,
+                widget.sessionValue,
                 style: AppTextStyles.body.copyWith(fontWeight: FontWeight.bold),
               ),
               SizedBox(width: 5.w),
@@ -48,34 +80,38 @@ class ListileWidget extends StatelessWidget {
               ),
               SizedBox(width: 5.w),
               Text(
-                percentage,
+                widget.percentage,
                 style: AppTextStyles.body.copyWith(fontWeight: FontWeight.bold),
               ),
             ],
           ),
         ],
       ),
-      subtitle: Container(
-        height: 8.h, // adjust as needed
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.r),
-          gradient: LinearGradient(
-            colors: [
-              AppColors.secondary,
-              AppColors.secondary,
-              AppColors.bodyColor.withOpacity(0.2),
-              AppColors.bodyColor.withOpacity(0.2),
-            ],
-            stops: [
-              0.0,
-              jaugeValue,
-              jaugeValue,
-              1.0,
-            ],
-          ),
-        ),
-      ),
+      subtitle: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Container(
+              height: 8.h, // adjust as needed
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.r),
+                gradient: LinearGradient(
+                  colors: [
+                    AppColors.secondary,
+                    AppColors.secondary,
+                    AppColors.bodyColor.withOpacity(0.2),
+                    AppColors.bodyColor.withOpacity(0.2),
+                  ],
+                  stops: [
+                    0.0,
+                    _animation.value,
+                    _animation.value,
+                    1.0,
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
